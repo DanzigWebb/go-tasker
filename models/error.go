45995335fed20ec84d1ce47132c642b2893da782
Alexandr <valverin95@gmail.com>
2021-05-30 18:54:53 +0300

    refactor: объект ошибки

--- models/error.go
+++ models/error.go
@@ -1,5 +1,13 @@
 package models
 
 type ApiError struct {
+	Error   bool   `json:"error" default:"true"`
 	Message string `json:"message"`
+}
+
+func DefaultError(msg string) ApiError {
+	return ApiError{
+		Error:   true,
+		Message: msg,
+	}
 }
